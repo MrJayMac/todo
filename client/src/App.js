816539921feebc0a76fb7ef333e9e6a2f6b9045d
MrJayMac <mac.joshua450@gmail.com>
2024-07-16 08:07:25 -0400

    connecting backend to front end, still need to fix password issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ListHeader from './components/ListHeader';
 
 function App() {
+
+  //HARD CODING EMAIL
+  const userEmail = 'joshua@test.com';
+
+  const [task, setTask] = useState(null);
+
+  const getData = async () => {
+
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(task)
+
   return (
     <div className="App">
-      <ListHeader listName={'Holiday Inn'}/>
+      <ListHeader />
+      {/* other components */}
     </div>
   );
 }
--- server/db.js
+++ server/db.js
@@ -1,6 +1,14 @@
 const Pool = require('pg').Pool
 require('dotenv').config()
 
+console.log('DB Config:', {
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
+    database: 'todoapp'
+});
+
 const pool = new Pool({
     user: process.env.USERNAME,
     password: process.env.PASSWORD,
