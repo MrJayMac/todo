7e180bf5beee70ba0e9e6c0685ceec085d223ab0
MrJayMac <mac.joshua450@gmail.com>
2024-07-25 19:55:35 -0400

    creating modal to create task

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Modal from './Modal'
 
 const ListHeader = ({listName}) => {
 
@@ -16,6 +17,7 @@ const ListHeader = ({listName}) => {
         <button className='create'>CREATE</button>
         <button className='signout' onClick={signOut}>SIGN OUT</button>
       </div>
+      <Modal/>
     </div>
   )
 }
---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Modal(){
+
+  const mode = 'edit'
+
+  const handleChange = () =>{
+    console.log('changing')
+  }
+
+  return (
+    <div className='overlay'>
+      <div className='modal'>
+        <div className='form-title-container'>
+          <h3>Let's {mode} your task</h3>
+          <button>X</button>
+        </div>
+
+        <form>
+          <input required maxLength={30} placeholder='Your task goes here' name='title' value={""} onChange={handleChange}/>
+          <br/>
+          <label for="range">Drag to select your current progress</label>
+          <input required type='range' id='range' min={0} max={100} name='progress' value={""} onClick={handleChange}/>
+
+          <input className={mode} type='submit'/>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
