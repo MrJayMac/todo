a58a79525d2cb9c9168fffa4b21a32eaa5e47c2f
MrJayMac <mac.joshua450@gmail.com>
2024-07-31 06:56:52 -0400

    finishing list header

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
 import Modal from './Modal'
+import { useState } from 'react'
+import { useCookies } from 'react-cookie'
 
-const ListHeader = ({listName}) => {
+const ListHeader = ({ listName, getData }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null)
+  const [showModal, setShowModal] = useState(false)
 
   const signOut = () => {
-    console.log("signout")
+    console.log('signout')
+    removeCookie('Email')
+    removeCookie('AuthToken')
+    window.location.reload()
+
   }
 
-
   return (
-    <div className='list-header'>
-      <h1>
-        {listName}
-      </h1>
-      <div className='button-container'>
-        <button className='create'>CREATE</button>
-        <button className='signout' onClick={signOut}>SIGN OUT</button>
+    <div className="list-header">
+      <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
+        <button className="signout" onClick={signOut}>SIGN OUT</button>
       </div>
-      <Modal mode={'create'}/>
+      {showModal && <Modal mode={'create'} setShowModal={setShowModal} getData={getData} />}
     </div>
   )
 }
