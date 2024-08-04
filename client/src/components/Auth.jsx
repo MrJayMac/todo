d0fbc7e7692b98020205e1926d794de478658067
MrJayMac <mac.joshua450@gmail.com>
2024-08-04 11:41:31 -0400

    getting started with auth

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 function Auth() {
+
+  const isLogIn = false
   return (
-    <div>Auth</div>
+    <div className='auth-container'>
+      <div className='auth-container-box'>
+        <form>
+          <h2>{isLogIn ? 'Please login':'Please Signup'}</h2>
+          <input type='email' placeholder='email'/>
+          <input type='password' placeholder='password'/>
+          {!isLogIn && <input type='password' placeholder='confirm password'/>}
+          <input type='submit' className='create'/>
+        </form>
+      </div>
+    </div>
   )
 }
 
